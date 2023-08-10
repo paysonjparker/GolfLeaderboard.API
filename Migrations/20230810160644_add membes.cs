a5c4656abe963888e6b5d8871a48ad3f6cf3ebd7
Payson Parker <payson.parker@costco.com>
2023-08-10 09:21:58 -0700

    Nerw migrastion and update databas e

---
+++ Migrations/20230810160644_add membes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GolfLeaderboard.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class addmembes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "GolfCourseId",
+                table: "Golfers",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Golfers_GolfCourseId",
+                table: "Golfers",
+                column: "GolfCourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Golfers_GolfCourses_GolfCourseId",
+                table: "Golfers",
+                column: "GolfCourseId",
+                principalTable: "GolfCourses",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Golfers_GolfCourses_GolfCourseId",
+                table: "Golfers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Golfers_GolfCourseId",
+                table: "Golfers");
+
+            migrationBuilder.DropColumn(
+                name: "GolfCourseId",
+                table: "Golfers");
+        }
+    }
+}
---
+++ Migrations/20230810162133_Undo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GolfLeaderboard.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Undo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
